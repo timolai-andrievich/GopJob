0c4aa0a281c2ffe39078f7e7fd33fb51c1473ef6
timolai-andrievich <timolai.andrievitch@ya.ru>
2022-06-28 16:42:36 +0300

    Implemented API part of the page

--- src/routes/hot.svelte
+++ src/routes/hot.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
+    import Vacancy from './lib/Vacancy.svelte';
+    import type { VacancyType } from './lib/VacancyType';
 
+    interface ApiResponse {
+        page: number,
+        page_count: number,
+        items_per_page: number,
+        took: number,
+        timed_out: boolean,
+        total: number,
+        results: Array<VacancyType>,
+    }
+
+    async function updateVacancies(): Promise<Array<VacancyType>>{
+        let response: Response = await fetch('https://www.themuse.com/api/public/jobs?page=1');
+        let json: ApiResponse = await response.json() as ApiResponse;
+        return json.results;
+    }
+
+    let vacancies: Array<VacancyType> = [];
+    updateVacancies().then(x => vacancies = x)
 </script>
 
-<p>Hot vacancies</p>
+<p>Hot vacancies:</p>
+<div id="vacancies-list">
+    {#each vacancies as vacancy}
+        <Vacancy data={vacancy} />
+    {/each}
+</div>
---
+++ src/routes/lib/Vacancy.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { VacancyType } from './VacancyType';
+
+    export let data: VacancyType;
+</script>
+
+<h1>{data.name}</h1>
+<p>{@html data.contents}</p>
