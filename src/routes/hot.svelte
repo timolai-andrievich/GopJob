967527f23c714d88f46c928a0ba2509a4e3fa8ab
OldCoachman <thedarkknight.vanerk@gmail.com>
2022-06-28 19:10:13 +0300

    produced style

--- src/routes/hot.svelte
+++ src/routes/hot.svelte
@@ -22,9 +22,45 @@
     updateVacancies().then(x => vacancies = x)
 </script>
 
-<p>Hot vacancies:</p>
-<div id="vacancies-list">
+<svelte:head>
+    <title>The hottest vacancies</title>
+</svelte:head>
+
+<a href="/">JobFinder</a>
+<img src="fire.png" alt="Fire logo"/>
+<h1>List of the hottest<br> vacancies so far.</h1>
+
+<div class="vacancies-list" id="vacancies-list">
     {#each vacancies as vacancy}
         <Vacancy data={vacancy} />
     {/each}
 </div>
+
+<style>
+    a {
+        font-family: "Brawler", sans-serif;
+        font-weight: bold;
+        font-size: 3.1vw;
+        text-decoration: none;
+        color: black;
+        position: absolute;
+        right: 5vw;
+        top: 10vh
+    }
+    img {
+        width: 5vw;
+        margin-top: 32.5vh;
+        margin-left: 3vw
+    }
+    h1 {
+        margin-top: -10vh;
+        margin-left: 8.5vw;
+        margin-bottom: 7vh;
+        font-family: 'Lato', sans-serif;
+        font-size: 2.1vw;
+    }
+    .vacancies-list {
+        margin-left: 2.5vw;
+        margin-right: 2.5vw;
+    }
+</style>
--- src/routes/lib/Vacancy.svelte
+++ src/routes/lib/Vacancy.svelte
@@ -4,5 +4,28 @@
     export let data: VacancyType;
 </script>
 
-<h1>{data.name}</h1>
-<p>{@html data.contents}</p>
+<div class="vacancy">
+    <h2>{data.name}</h2>
+    <p>{@html data.contents}</p>
+</div>
+
+
+<style>
+    .vacancy {
+        background-color: #d0d2db;
+        border-radius: 50px;
+        margin-bottom: 7vh;
+    }
+    h2, p {
+        font-family: 'Lato', sans-serif;
+    }
+    h2 {
+        padding-top: 7vh;
+        padding-left: 4vw;
+    }
+    p {
+        padding-left: 5.9vw;
+        padding-bottom: 7vh;
+        padding-right: 4vw
+    }
+</style>
