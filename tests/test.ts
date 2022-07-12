2425ba139bc515041a831e6beb45f2cfaf1238ec
timolai-andrievich <timolai.andrievitch@ya.ru>
2022-07-12 21:26:19 +0300

    Added unit tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('Two pages are available', async ({ page }) => {
+test('All of the pages are available', async ({ page }) => {
 	await page.goto('/');
 	await page.goto('/hot');
+	await page.goto('/filter');
 });
